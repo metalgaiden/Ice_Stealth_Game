2eca8b8065579d3aa0fac9eb9384e9aa039fe678
metalgaiden <musicalmiller@gmail.com>
2020-03-26 23:14:29 -0700

    finished boss fight mechanics

--- Enemies/Tank.gd
+++ Enemies/Tank.gd
@@ -5,11 +5,15 @@ var speed = 70
 var velocity = Vector2()
 var acceleration = Vector2()
 var steer_force = 10
+var l = false
+var r = false
 
 func start(_target = null):
 	target = _target
 
 func _process(delta):
+	if l == true && r == true:
+		print('boss killed')
 	if ((target.position - position).length() > 300):
 		acceleration += seek()
 		velocity += acceleration * delta
@@ -20,6 +24,22 @@ func _process(delta):
 	position += velocity * delta
 
 func seek():
-    var desired = (target.position - position).normalized() * speed
-    var steer = (desired - velocity).normalized() * steer_force
-    return steer
+	var desired = (target.position - position).normalized() * speed
+	var steer = (desired - velocity).normalized() * steer_force
+	return steer
+
+
+func _on_Right_wheel_area_entered(area):
+	if area.name == 'b':
+		if area.get_parent().i == true:
+			$"Right wheel/Sprite".self_modulate.r = 0
+			area.get_parent().queue_free()
+			r = true
+
+
+func _on_left_wheel_area_entered(area):
+	if area.name == 'b':
+		if area.get_parent().i == true:
+			$"left wheel/Sprite".self_modulate.r = 0
+			area.get_parent().queue_free()
+			l = true
