912b1206f6b9ff5d6cb44e87860130f011c42c54
metalgaiden <musicalmiller@gmail.com>
2020-07-27 15:08:21 -0700

    boss missiles added

--- Enemies/Tank.gd
+++ Enemies/Tank.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+export (PackedScene) var Missile
 var target
 var speed = 70
 var velocity = Vector2()
@@ -10,10 +11,11 @@ var r = false
 
 func start(_target = null):
 	target = _target
+	$Timer.start()
 
 func _process(delta):
 	if l == true && r == true:
-		print('boss killed')
+		pass
 	if ((target.position - position).length() > 300):
 		acceleration += seek()
 		velocity += acceleration * delta
@@ -28,18 +30,32 @@ func seek():
 	var steer = (desired - velocity).normalized() * steer_force
 	return steer
 
-
 func _on_Right_wheel_area_entered(area):
-	if area.name == 'b':
+	if r == true:
+		pass
+	elif area.name == 'b':
 		if area.get_parent().i == true:
 			$"Right wheel/Sprite".self_modulate.r = 0
+			steer_force -= 5
 			area.get_parent().queue_free()
 			r = true
 
-
 func _on_left_wheel_area_entered(area):
-	if area.name == 'b':
+	if l == true:
+		pass
+	elif area.name == 'b':
 		if area.get_parent().i == true:
 			$"left wheel/Sprite".self_modulate.r = 0
+			steer_force -= 5
 			area.get_parent().queue_free()
 			l = true
+
+func shoot(pos):
+	var m = Missile.instance()
+	var a = (pos - global_position).angle()
+	m.start(global_position, a, target)
+	get_parent().add_child(m)
+
+func _on_Timer_timeout():
+	shoot(target.position)
+	$Timer.start()
--- Game over.gd
+++ Game over.gd
@@ -11,8 +11,8 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed('ui_accept'):
-		get_tree().change_scene("Main.tscn")
+		get_tree().change_scene("res://Levels/Main.tscn")
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("Main.tscn")
+	get_tree().change_scene("res://Levels/Main.tscn")
